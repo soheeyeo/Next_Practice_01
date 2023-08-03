f7c18d57558536482acffacf8c6241b2e2836c06
soheeyeo <sohee407@gmail.com>
2023-08-03 18:57:44 +0900

    Initial setting

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  let name = 'yeo';
+
+  return (
+    <div>
+      <h4 className="title">로켓프레시</h4>
+      <p className="title-sub">by dev { name }</p>
+    </div>
+  )
+}
