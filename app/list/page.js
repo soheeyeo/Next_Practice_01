11a9f3aa936c3640569c02a16b0466c865d71087
soheeyeo <sohee407@gmail.com>
2023-08-03 19:52:29 +0900

    map

--- app/list/page.js
+++ app/list/page.js
@@ -1,14 +1,18 @@
 export default function List() {
+    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
 
     return (
         <div>
             <h4 className="title">상품목록</h4>
-            <div className="food">
-                <h4>상품1 $40</h4>
-            </div>
-            <div className="food">
-                <h4>상품2 $40</h4>
-            </div>
+            {
+                상품.map((a, i)=>{
+                    return (
+                        <div className="food">
+                            <h4>{a} $40</h4>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
