702bb11e38c8b97e93536a5027dc292d756f0092
soheeyeo <sohee407@gmail.com>
2023-08-04 18:38:38 +0900

    props

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,21 +1,19 @@
-import Hello from "./data"
-
 export default function Cart() {
+    let 장바구니 = ['Tomatoes', 'Pasta']
+
     return (
         <div>
         <h4 className="title">Cart</h4>
-        <Hello/>
-        <CartItem/>
-        <CartItem/>
-        <CartItem/>
+        <CartItem item={장바구니[0]} />
+        <CartItem item={장바구니[1]} />
         </div>
     )
 } 
 
-function CartItem() {
+function CartItem(props) {
     return (
         <div className="cart-item">
-            <p>상품명</p>
+            <p>{props.item}</p>
             <p>$40</p>
             <p>1개</p>
         </div>
