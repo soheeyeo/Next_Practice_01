68defc312cf3d18c8e7f9f1b2fcdcbb65319455a
soheeyeo <sohee407@gmail.com>
2023-08-04 19:43:41 +0900

    props 02

--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,10 @@
+'use client'
+
+import { useState } from "react"
+
 export default function List() {
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+    let [수량, 수량변경] = useState(0)
 
     return (
         <div>
@@ -10,6 +15,9 @@ export default function List() {
                         <div className="food" key={i}>
                             <img src={`./food${i}.png`} className="food-img" />
                             <h4>{a} $40</h4>
+                            <button onClick={()=>{ 수량변경(수량-1) }}>-</button>
+                            <span> {수량} </span>
+                            <button onClick={()=>{ 수량변경(수량+1) }}>+</button>
                         </div>
                     )
                 })
